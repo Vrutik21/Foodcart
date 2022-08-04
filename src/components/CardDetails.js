6691dd38381dd58ecf8d705101dd4a2b5d3dabfc
Vrutik Parmar <parmarvrutik1@gmail.com>
2022-08-04 16:25:42 +0530

    'final1'

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -29,7 +29,7 @@ const CardDetails = () => {
 
   const dlt = (id) => {
     dispatch(DEL(id));
-    history("/eCommerce-vrutik/");
+    history("/eCommerce-vrutik");
   };
 
   const send = (e) => {
